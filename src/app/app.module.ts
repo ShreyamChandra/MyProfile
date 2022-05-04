305ecc6cbb88f90fc11504e792aa28b388b011f9
ShreyamChandra <95626322+ShreyamChandra@users.noreply.github.com>
2022-05-04 16:52:32 +0530

    final draft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AngularEmojisModule } from 'angular-emojis';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
@@ -26,7 +26,8 @@ import { ProjectsComponent } from './projects/projects.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularEmojisModule
   ],
   providers: [],
   bootstrap: [AppComponent]
